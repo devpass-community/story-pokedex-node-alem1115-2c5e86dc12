8c60d0390a9d72c232b03e058542ad3adb99ee73
Alessandro Marchesin <a.marchesin@epsonemear.com>
2023-05-17 14:59:23 +0200

    Should work now

--- src/controllers/pokedexController.js
+++ src/controllers/pokedexController.js
@@ -4,8 +4,7 @@ const getPokemon = async (req, res, next) => {
     const { id } = req.params;
 
     const pokemon = await service.getPokemon(id);
-    console.log(pokemon);
-    return res.json(pokemon.name); 
+    return res.json(pokemon.name);
 };
 
 module.exports = {
--- src/services/pokedexService.js
+++ src/services/pokedexService.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
+const https = require('https');
+
 const getPokemon = async (id) => {
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+    });
+
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
     let result;
-    await axios
-        .get(apiUrl)
-        .then(
-            function (response) {
-                try {
-                    return response.data
-                } catch {
-                    console.log(error)
-                }
-            }
-        );
+    try {
+        const response = await axios.get(apiUrl, { httpsAgent: agent });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 
-    return result;
 };
 
 module.exports = {
